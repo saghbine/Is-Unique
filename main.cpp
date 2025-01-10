dd34a80ae1a2f255bbd9ac787234bace5b8416b6
Daniel Saghbini <134181843+saghbine@users.noreply.github.com>
2025-01-10 11:20:54 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@
 //  Is Unique
 //
 //  Created by daniel saghbine on 12/8/24.
-//
-//  CTCI 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
-//  Auxiliary space complexity: O(1). Time complexity: O(n log n).
+//  CTCI 1.1 Is Unique
 
 #include <iostream>
 using namespace std;
@@ -16,10 +14,14 @@ int main()
 {
     string str;
     
-    cout<<"Type a string: ";
+    cout<<"problem\n-------\n\tImplement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?\n\n";
+    
+    cout<<"input\n-----\n\tType: ";
     cin>>str;
     
-    cout<<"The string is"<<(Is_Unique(str) ? "" : "n't")<<" unique.\n";
+    cout<<"\noutput\n------\n\tThe string is"<<(Is_Unique(str) ? "" : "n't")<<" unique.\n\n";
+    
+    cout<<"solution\n--------\n\tAuxiliary space complexity: O(1); time complexity: O(n log n).\n\n";
     
     return 0;
 }
@@ -84,15 +86,15 @@ void heapsort(string &s)
     }
 }
 
-bool Is_Unique(string &s) // s is a C++ string
+bool Is_Unique(string &s) // s is input string
 {
-    heapsort(s); // sort the string using heapsort
+    heapsort(s); // sort s using heapsort
     
-    for(int i=0; i < s.size()-1; i++) // check char at indices 0...N-2 in the string
+    for(int i=0; i < s.size()-1; i++) // check char at indices 0...N-2 in s
     {
         if(s[i] == s[i+1]) // check if adjacent char pairs are equivalent
-            return false; // the string isn't unique
+            return false; // s isn't unique
     }
     
-    return true; // the string is unique
+    return true; // s is unique
 }
